773ef751f9359e216a257893e2e9fb1ec535d704
AnandEmbold <80742162+AnandEmbold@users.noreply.github.com>
2021-08-17 13:35:25 +0530

    Update FinalizeWithoutSuper.java

--- FinalizeWithoutSuper.java
+++ FinalizeWithoutSuper.java
@@ -2,8 +2,8 @@
 class FinalizeWithoutSuper {
     protected void finalize() {
 		//Comment/Uncomment to check
-		super.finalize();
-		int i = 0;
 		//super.finalize();
+		int i = 0;
+		super.finalize();
 	}
 }
