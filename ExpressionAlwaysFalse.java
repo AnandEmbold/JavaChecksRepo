b075f641ed4079fc94b212c4404f9ae141007de7
AnandEmbold <80742162+AnandEmbold@users.noreply.github.com>
2021-08-17 11:20:10 +0530

    Update ExpressionAlwaysFalse.java
    Compilation err

--- ExpressionAlwaysFalse.java
+++ ExpressionAlwaysFalse.java
@@ -1,56 +1,76 @@
 
 class ExpressionAlwaysFalse {
     public void someCheck(boolean b) {
-		if(b == true) 
-			int i=0;
+		int  i = -1;
+		if(b == true) {
+			i=0;
+		}
 		
 		boolean b2 = false;
-		if(b2)
-			int i=0;
-		if(b2!=true)
-			int i=0;
-		if(!b2)
-			int i=0;
+		if(b2) {
+			i=0;
+		}
+		if(b2!=true){
+			i=0;
+		}
+		if(!b2){
+			i=0;
+		}
 		
 		boolean b3 = true;
-		if(!b3)
-			int i=0;
-		if(b3!=true)
-			int i=0;
-		if(b3)
-			int i =0;
+		if(!b3){
+			i=0;
+		}
+		if(b3!=true){
+			i=0;
+		}
+		if(b3){
+			i =0;
+		}
 		
 		b3 = false;
-		if(!b3)
-			int i=0;
-		if(b3!=true)
-			int i=0;
-		if(b3)
-			int i =0;
+		if(!b3){
+			i=0;
+		}
+		if(b3!=true){
+			i=0;
+		}
+		if(b3){
+			i =0;
+		}
 		
 		Boolean b4= true;
-		if(b4)
-			int i=0;
-		if(b4!=true)
-			int i=0;
-		if(!b4)
-			int i=0;
+		if(b4){
+			i=0;
+		}
+		if(b4!=true){
+			i=0;
+		}
+		if(!b4){
+			i=0;
+		}
 		
 		Boolean b5 = true;
-		if(!b5)
-			int i=0;
-		if(b5!=true)
-			int i=0;
-		if(b5)
-			int i =0;
+		if(!b5){
+			i=0;
+		}
+		if(b5!=true){
+			i=0;
+		}
+		if(b5){
+			i =0;
+		}
 		
 		Boolean b6 = null;
-		if(!b6)
-			int i=0;
-		if(b6!=true)
-			int i=0;
-		if(b6)
-			int i =0;
+		if(!b6){
+			i=0;
+		}
+		if(b6!=true){
+			i=0;
+		}
+		if(b6){
+			i =0;
+		}
 		
 	}
 }
